c0a8ebc24bdb1523d63529e5340e7e892eff2aeb
Viorel Petcu <viorel.pe@gmail.com>
2017-07-16 20:44:30 +0200

    add headers and rename json fields

--- server/sound_bites.js
+++ server/sound_bites.js
@@ -1,13 +1,13 @@
 [
-{"id": 0, "name": "1_unacceptable - loud.mp3"},
-{"id": 1, "name": "aaand its gone - loud.mp3"},
-{"id": 2, "name": "dough - loud.mp3"},
-{"id": 3, "name": "get to the choppa - loud.mp3"},
-{"id": 4, "name": "I'm Mr. Meeseeks, look at me!.mp3"},
-{"id": 5, "name": "IT-Crowd-On-Off.mp3"},
-{"id": 6, "name": "move along - loud.mp3"},
-{"id": 7, "name": "Seinfeld Noise.mp3"},
-{"id": 8, "name": "Tea. Earl Grey - loud.mp3"},
-{"id": 9, "name": "very little - loud.mp3"},
-{"id": 10, "name": "Wubba Lubba Dub Dub.mp3"}
+{"id": 0, "title": "1_unacceptable - loud.mp3"},
+{"id": 1, "title": "aaand its gone - loud.mp3"},
+{"id": 2, "title": "dough - loud.mp3"},
+{"id": 3, "title": "get to the choppa - loud.mp3"},
+{"id": 4, "title": "I'm Mr. Meeseeks, look at me!.mp3"},
+{"id": 5, "title": "IT-Crowd-On-Off.mp3"},
+{"id": 6, "title": "move along - loud.mp3"},
+{"id": 7, "title": "Seinfeld Noise.mp3"},
+{"id": 8, "title": "Tea. Earl Grey - loud.mp3"},
+{"id": 9, "title": "very little - loud.mp3"},
+{"id": 10, "title": "Wubba Lubba Dub Dub.mp3"}
 ]
