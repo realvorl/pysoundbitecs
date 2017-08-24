0051e33defb16b6ccb6de65147a2ad2dd4814635
Viorel Petcu <viorel.pe@gmail.com>
2017-08-25 01:06:20 +0200

    update documentation and indent

--- readme.md
+++ readme.md
@@ -13,7 +13,8 @@ in case you missed it, you will need `web.py`
     sudo apt install python-pip
     pip install web.py
 
-This web service, once started, will list the content of the `/mp3` folder and transform the filenames into a JSon Array together with the associated images from the `/images` folder.
+This service, once started, will list the content of the `/mp3` folder and transform the filenames 
+into a JSon Array together with the associated images from the `/static/images` folder.
 
 The Code is setup to display any `.mp3` file as a tile that is represented in `final product.png`
 
@@ -25,6 +26,11 @@ The necessary fields are modeled in this class:
     		self.title = title
     		self.img = img
 
+The actual MP3's are played using `xmms2`, here is how you can install it:
+
+	sudo apt install xmms2
+	man xmms2
+
 To be able to run the python script, you must ensure that the file is executable:
 
     chmod +x rest.py
@@ -33,10 +39,6 @@ After that you run it using:
 
 	./rest.py
 
-The actual MP3's are played using `xmms2`, here is how you can install it:
-
-	sudo apt install xmms2
-	man xmms2
 
 ### For the client part:
 
@@ -47,15 +49,24 @@ we are going to use REACT.JS with BABEL-CORE and jQuery:
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.34/browser.js"></script>
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
-If all went well you will be able to access / play the mp3's by opening your browser and visiting this adress: 
+To build the frontend:
 
-[http://localhost:8080](URL)
+- navigate to `sound-biter/`
+- run: `npm install`
+- run: `npm start`
+
+if your default browser does not pop up, you can access:
+
+[http://localhost:3000](URL)
+
+
+###How To Use:
 
 Adding mp3's to `/mp3` folder will add new buttons:
 
     server/mp3/I'm Mr. Meeseeks, look at me!.mp3
 
-Adding an image with the same name as the mp3 to wich it belongs, will display it on the button instead of the fallback image:
+Adding an image with the same name as the mp3 to which it belongs, will display it on the button instead of the fallback image:
 
     server/images/I'm Mr. Meeseeks, look at me!.mp3.jpg
 
--- sound-biter/src/BuzzerButton.js
+++ sound-biter/src/BuzzerButton.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg'
 import Draggable from 'react-draggable'
 import jquery from 'jquery'
 import './App.css'
@@ -8,38 +7,38 @@ var soundBiteServer = "http://localhost:8080"
 var defaultImg = 'default.png'
 
 var BuzzerButton = React.createClass({
-  getInitialState() {
-    return {count: 0}
-  },
+    getInitialState() {
+        return {count: 0}
+    },
 
-  handlePlay() {
-    var bThis = this;
-    var biteId = bThis.props.order;
-    var url = soundBiteServer+"/soundBite/"+biteId;
-    jquery.getJSON(url, function(data) {
-     bThis.setState({count: (bThis.state.count + 1) })
-    })
-  },
-  handleRemove() {
-    console.log("removing" + this.props.title);
-    this.setState({off: true});
-  },
+    handlePlay() {
+        var bThis = this;
+        var biteId = bThis.props.order;
+        var url = soundBiteServer+"/soundBite/"+biteId;
+        jquery.getJSON(url, function(data) {
+            bThis.setState({count: (bThis.state.count + 1) })
+        })
+    },
+    handleRemove() {
+        console.log("removing" + this.props.title);
+        this.setState({off: true});
+    },
 
-  render() {
-    if (this.state.off) return null;
-    return (
-      <Draggable>
-      <div className="bimmel" >
-      <h3>{this.props.title}</h3>
-      <img src={"http://localhost:8080/static/images/" + this.props.img} onClick={this.handlePlay}></img>
-      <span>
-        <span className="counter"> #{this.state.count} </span>
-        <button onClick={this.handleRemove}> x </button>
-      </span>
-    </div>
-    </Draggable>
-    )
-  }
+    render() {
+        if (this.state.off) return null;
+        return (
+            <Draggable>
+              <div className="bimmel" >
+                <h3>{this.props.title}</h3>
+                <img src={"http://localhost:8080/static/images/" + this.props.img} onClick={this.handlePlay}></img>
+                <span>
+                  <span className="counter"> #{this.state.count} </span>
+                  <button onClick={this.handleRemove}> x </button>
+                </span>
+              </div>
+            </Draggable>
+        )
+    }
 })
 
 export default BuzzerButton
